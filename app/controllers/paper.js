fb4dfee2ff44ec030622adbbce96635c2b92e8ae
jasonhsieh <appleoxxo@gmail.com>
2016-08-11 18:02:15 +0800

    sequilize update and upsert

--- app/controllers/paper.js
+++ app/controllers/paper.js
@@ -17,12 +17,116 @@ router.get('/papers', function (req, res, next) {
 });
 
 
+router.get('/test', function (req, res, next) {
+  console.log(123);
+  db.Paper.findAll().then(function (papers) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+
+    res.json(papers);
+  });
+});
+
+// update single todo
+router.put('/todo/:id', function(req, res) {
+  models.Todo.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(todo) {
+    if(todo){
+      todo.updateAttributes({
+        title: req.body.title,
+        complete: req.body.complete
+      }).then(function(todo) {
+        res.send(todo);
+      });
+    }
+  });
+});
+
+router.post('/test', function (req, res, next) {
+
+  var updatedData = JSON.parse(req.body.data);
+
+
+function storeData(index){
+  db.Paper.find({
+    where:{
+      id : updatedData[index].id
+    }
+    }).then(function(paper){
+      paper.updateAttributes({
+        fromtext:updatedData[index].fromtext,
+        fromnumber:updatedData[index].fromnumber,
+        totext:updatedData[index].totext,
+        tonumber:updatedData[index].tonumber,
+        summary:updatedData[index].summary
+      });
+    });
+}
+  for(i in updatedData){
+    storeData(i);
+  }
+  //允許CORS
+  res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+  //end 允許CORS
+
+  res.send(req.body);
+});
+
 router.get('/getpapers', function (req, res, next) {
   db.Paper.findAll().then(function (papers) {
 
     res.json(papers);
   });
 });
+
+
+router.post('/test2', function (req, res, next) {
+
+console.log("test222");
+//// Sequilize 的 upsert 方法 ， 如果是新的資料的話 就新增，如果不是的話 就 更新
+
+        var updatedData = JSON.parse(req.body.data);
+
+
+          function storeData(index){
+            db.Paper.upsert(
+              updatedData[index]
+            ).then(function(paper){
+                console.log("successfully!!");
+              });
+            }
+
+            for(i in updatedData){
+              storeData(i);
+            }
+
+            //允許CORS
+            res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+            res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+            //end 允許CORS
+
+            res.send(req.body);
+
+          });
+
+
+
+
+
+router.get('/getpapers', function (req, res, next) {
+  db.Paper.findAll().then(function (papers) {
+
+    res.json(papers);
+  });
+});
+
 
 // router.get('/paper:id', function (req, res, next) {
 //   models.Todo.find({
@@ -58,10 +162,7 @@ router.post('/paper', function (req, res, next) {
   // res.send(req.body);
 });
 
-router.post('/test', function (req, res, next) {
-  console.log(req.body.name);
-  res.send(req.body.name);
-});
+
 
 
 //
--- config/express.js
+++ config/express.js
@@ -59,4 +59,13 @@ module.exports = function(app, config) {
       });
   });
 
+
+  //allow cross domain access
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    next();
+});
+
 };
